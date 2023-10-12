f1c76cd1bb69548dbc68336a601683563b79ce04
solomon <solonjogo@gmail.com>
2023-10-11 17:15:07 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - print _putchar
- * return: always 0
+ * main - Print _putchar
+ * Return: Always 0
  */
-int mian (void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
