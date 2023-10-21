b7141c8d67547900521d46e93da5fc31697415c5
solomon <solonjogo@gmail.com>
2023-10-20 17:15:41 -0700

    fix bugs

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,25 +7,25 @@
  */
 char *cap_string(char *s)
 {
-  int i = 0, j;
-  char a[] = " \t\n,;.!?\"(){}";
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
-  while (*(s + i))
-    {
-      if (*(s + i) >= 'a' && *(s + i) <= 'z')
+	while (*(s + i))
 	{
-	  if (i == 0)
-	    *(s + i) -= 'a' - 'A';
-	  else
-	    {
-	      for (j = 0; j <= 12; j++)
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-		  if (a[j] == *(s + i - 1))
-		    *(s + i) -= 'a' - 'A';
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
+			}
 		}
-	    }
+		i++;
 	}
-      i++;
-    }
-  return (s);
+	return (s);
 }
