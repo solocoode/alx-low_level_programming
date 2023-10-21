fe8978d4dff0b0672c9433b6619db6eda21daf25
solomon <solonjogo@gmail.com>
2023-10-20 17:16:53 -0700

    fix bugs

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,17 +8,17 @@
  */
 char *leet(char *s)
 {
-  int i, j;
-  char a[] = "aAeEoOtTlL";
-  char b[] = "4433007711";
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
 
-  for (i = 0; *(s + i); i++)
-    {
-      for (j = 0; j <= 9; j++)
+	for (i = 0; *(s + i); i++)
 	{
-	  if (a[j] == s[i])
-	    s[i] = b[j];
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == s[i])
+				s[i] = b[j];
+		}
 	}
-    }
-  return (s);
+	return (s);
 }
