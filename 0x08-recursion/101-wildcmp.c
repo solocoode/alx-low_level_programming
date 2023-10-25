dd99c3e0a010fe8a2f23b126dafdae7e18167c23
solomon <solonjogo@gmail.com>
2023-10-25 12:24:37 -0700

    fix betty style

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,15 +10,15 @@
  */
 int str_checker(char *s1, char *s2, int i, int j)
 {
-  if (s1[i] == '\0' && s2[j] == '\0')
-    return (1);
-  if (s1[i] == s2[j])
-    return (str_checker(s1, s2, i + 1, j + 1));
-  if (s1[i] == '\0' && s2[j] == '*')
-    return (str_checker(s1, s2, i, j + 1));
-  if (s2[j] == '*')
-    return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
-  return (0);
+	if (s1[i] == '\0' && s2[j] == '\0')
+		return (1);
+	if (s1[i] == s2[j])
+		return (str_checker(s1, s2, i + 1, j + 1));
+	if (s1[i] == '\0' && s2[j] == '*')
+		return (str_checker(s1, s2, i, j + 1));
+	if (s2[j] == '*')
+		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+	return (0);
 }
 /**
  * wildcmp - check if strings could be considered identical
@@ -29,5 +29,5 @@ int str_checker(char *s1, char *s2, int i, int j)
  */
 int wildcmp(char *s1, char *s2)
 {
-  return (str_checker(s1, s2, 0, 0));
+	return (str_checker(s1, s2, 0, 0));
 }
