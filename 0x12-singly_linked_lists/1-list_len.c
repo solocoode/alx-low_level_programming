db46eaab20aa4120087d156adaa61868f94cafec
solomon <solonjogo@gmail.com>
2023-11-16 11:02:26 -0800

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,21 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * list_len - check the code for Holberton School students.
- * @h: name of the list
- * Return: the number of nodes.
- */
-size_t list_len(const list_t *h)
-{
-	int count = 0;
+ * list_len - finds the number of elements in a linked list
+ *@h: pointer to the list
+ *
+ *Return: the number of elements
+*/
 
-	while (h)
+size_t list_len(const list_t *h)
 	{
-		count++;
-		h = h->next;
+		size_t elements = 0;
+
+		while (h)
+		{
+			elements++;
+			h = h->next;
+		}
+
+		return (elements);
 	}
-	return (count);
-}
