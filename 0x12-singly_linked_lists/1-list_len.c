92fd514bdbd68d74cb7af21d919f28b15de538ca
solomon <solonjogo@gmail.com>
2023-11-16 09:41:52 -0800

    singly linked lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,24 +4,16 @@
 #include "lists.h"
 
 /**
- * print_list - check the code for Holberton School students.
+ * list_len - check the code for Holberton School students.
  * @h: name of the list
  * Return: the number of nodes.
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	int count = 0;
 
 	while (h)
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-		printf("[%d] %s\n", h->len, h->str);
-		}
 		count++;
 		h = h->next;
 	}
